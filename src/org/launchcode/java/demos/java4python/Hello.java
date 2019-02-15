135ceb898300b4b7e9babf38a7d6b341012f3c9b
Luke Kemner <kemnerl@gmail.com>
2019-02-15 15:52:58 -0500

    did exercise 1

--- src/org/launchcode/java/demos/java4python/Hello.java
+++ src/org/launchcode/java/demos/java4python/Hello.java
@@ -1,10 +1,24 @@
 package org.launchcode.java.demos.java4python;
 
+import java.util.Scanner;
+
 /**
  * From "Java for Python Programmers"
  */
 public class Hello {
+	
+	
+	public void name() {
+		Scanner scanner = new Scanner(System.in);
+    	System.out.println("Please Enter your full name");
+		String name =scanner.nextLine();
+		System.out.println("Hello "+name);
+	}
+	
+
+	
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+    	Hello hello = new Hello();
+    	hello.name();
     }
 }
