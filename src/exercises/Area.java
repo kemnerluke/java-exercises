d1ebc1368f42ebd886d480fd53e2fdc4fa9a2fd5
Luke Kemner <kemnerl@gmail.com>
2019-02-15 16:03:15 -0500

    exercise 2 complete

--- src/exercises/Area.java
+++ src/exercises/Area.java
@@ -1,10 +1,26 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class Area {
+	
+	
+	public void calculateArea() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("what is the width of your rectangle");
+		double width =scanner.nextDouble();
+		System.out.println("what is the height of your rectangle");
+		double height=scanner.nextDouble();
+		 double area =width * height;
+		 
+		 System.out.println("AREA: "+area);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Area area= new Area();
+		area.calculateArea();
+		
 	}
 
 }
