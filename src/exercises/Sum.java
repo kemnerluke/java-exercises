436b8f1ad0c084078c96dac8e5adf94efdd454f6
Luke Kemner <kemnerl@gmail.com>
2019-02-21 10:31:32 -0500

    CLASS 2 PREP EXERCISES

---
+++ src/exercises/Sum.java
@@ -0,0 +1,39 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum {
+	
+	
+	public static void even(int[] obj) {
+		
+		int sum=0;
+		
+		
+		for(int i=0;i<obj.length;i++) {
+			
+		
+			int number =obj[i];
+			
+			if(!(number%2==0)) {
+				System.out.println(i+" is a even number:");
+				sum+=i;
+
+			}
+			else {
+				System.out.println("\n"+i+" is not a even number");
+			}
+		 }
+			System.out.println("SUM "+sum);
+
+	}
+
+	public static void main(String[] args) {
+		int array[]= {1,2,3,4,5,6,7,8,9};
+
+		
+		even(array);
+	}
+
+}
